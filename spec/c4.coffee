4ae78a1184279287ae54b65a3e7df8e511e98566
Steve <steve.hook@gmail.com>
2011-07-28 18:29:38 +0100

    Some specs for the move method

--- spec/c4.coffee
+++ spec/c4.coffee
@@ -2,5 +2,16 @@ describe 'C4', ->
   beforeEach ->
     window.gameBoard = new GameBoard($('#canvas'), $('#canvasOverlay'), { })
   it 'gameBoard should have been created', ->
-    expect(window.gameBoard).toBeDefined()
+    expect(gameBoard).toBeDefined()
+  it 'first move should be red', ->
+    gameBoard.move 0
+    expect(gameBoard.grid).toEqual([['red'], [], [], [], [], [], []])
+  it 'second move should be yellow', ->
+    gameBoard.move 0
+    gameBoard.move 1
+    expect(gameBoard.grid).toEqual([['red'], ['yellow'], [], [], [], [], []])
+  it 'second move in the same column should be in second row', ->
+    gameBoard.move 0
+    gameBoard.move 0
+    expect(gameBoard.grid).toEqual([['red', 'yellow'], [], [], [], [], [], []])
 
--- spec/c4.js
+++ spec/c4.js
@@ -2,7 +2,21 @@ describe('C4', function() {
   beforeEach(function() {
     return window.gameBoard = new GameBoard($('#canvas'), $('#canvasOverlay'), {});
   });
-  return it('gameBoard should have been created', function() {
-    return expect(window.gameBoard).toBeDefined();
+  it('gameBoard should have been created', function() {
+    return expect(gameBoard).toBeDefined();
+  });
+  it('first move should be red', function() {
+    gameBoard.move(0);
+    return expect(gameBoard.grid).toEqual([['red'], [], [], [], [], [], []]);
+  });
+  it('second move should be yellow', function() {
+    gameBoard.move(0);
+    gameBoard.move(1);
+    return expect(gameBoard.grid).toEqual([['red'], ['yellow'], [], [], [], [], []]);
+  });
+  return it('second move in the same column should be in second row', function() {
+    gameBoard.move(0);
+    gameBoard.move(0);
+    return expect(gameBoard.grid).toEqual([['red', 'yellow'], [], [], [], [], [], []]);
   });
 });
