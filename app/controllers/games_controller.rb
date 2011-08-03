402d55728b4a60f932e4dd567e11f221ad3b8592
Steve Hook <steve.hook@gmail.com>
2011-08-03 21:19:47 +0100

    Renamed play view to show

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,4 +2,18 @@ class GamesController < ApplicationController
   def index
 
   end
+
+  def create
+    @game = Game.new(params[:game])
+
+    if @game.save
+      redirect_to(@game, :notice => 'Game was successfully created.')
+    else
+      redirect_to root_path
+    end
+  end
+
+  def show
+    
+  end
 end
