862880b5548c4c5854da6662b5b9299edac2d293
Steve Hook <steve.hook@gmail.com>
2011-08-03 18:22:59 +0100

    Jasmine config tidied up after some experimentation - still not working automatically - will need to revisit at a later stage

---
+++ spec/assets/javascripts/c4_spec.js
@@ -0,0 +1,40 @@
+(function() {
+  describe('C4', function() {
+    beforeEach(function() {
+      return window.gameBoard = new GameBoard($('#canvas'), $('#canvasOverlay'), {});
+    });
+    it('gameBoard should have been created', function() {
+      return expect(gameBoard).toBeDefined();
+    });
+    it('first move should be red', function() {
+      gameBoard.move(0);
+      return expect(gameBoard.grid).toEqual([['red'], [], [], [], [], [], []]);
+    });
+    it('second move should be yellow', function() {
+      gameBoard.move(0);
+      gameBoard.move(1);
+      return expect(gameBoard.grid).toEqual([['red'], ['yellow'], [], [], [], [], []]);
+    });
+    it('second move in the same column should be in second row', function() {
+      gameBoard.move(0);
+      gameBoard.move(0);
+      return expect(gameBoard.grid).toEqual([['red', 'yellow'], [], [], [], [], [], []]);
+    });
+    it('mouse click on the first column should trigger a move', function() {
+      var event;
+      console.log($('#canvasOverlay'));
+      event = new $.Event('click');
+      event.offsetX = 0;
+      $('#canvasOverlay').trigger(event);
+      return expect(gameBoard.grid).toEqual([['red'], [], [], [], [], [], []]);
+    });
+    return it('mouse click on the second column should trigger a move', function() {
+      var event;
+      console.log($('#canvasOverlay'));
+      event = new $.Event('click');
+      event.offsetX = 100;
+      $('#canvasOverlay').trigger(event);
+      return expect(gameBoard.grid).toEqual([[], ['red'], [], [], [], [], []]);
+    });
+  });
+}).call(this);
