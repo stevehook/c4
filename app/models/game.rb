87edf3e3b5a015a4d6caf989b7465de05117ed2d
Steve Hook <steve.hook@gmail.com>
2011-08-07 20:10:53 +0100

    Save the moves and grid fields as serialized JSON (rather than YAML)

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,7 @@
 class Game < ActiveRecord::Base
   after_initialize :init
+  before_save :before_save
+  after_save :after_save
 
   def init
     self.status = :in_play
@@ -11,5 +13,15 @@ class Game < ActiveRecord::Base
     # TODO: This is just a placeholder for now...
     self.moves << ['yellow', 0]
     self.grid[0] << 'yellow'
+  end
+
+  def before_save
+    self.moves = self.moves.to_json
+    self.grid = self.grid.to_json
+  end
+
+  def after_save
+    self.moves = JSON.parse(self.moves)
+    self.grid = JSON.parse(self.grid)
   end
 end
