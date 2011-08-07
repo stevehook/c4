1b51a9f6de408c1570bfe1784750aaa1629bfc67
Steve Hook <steve.hook@gmail.com>
2011-08-07 20:00:38 +0100

    Fix failing spec

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,11 @@
 class Game < ActiveRecord::Base
+  after_initialize :init
+
+  def init
+    puts 'init'
+    self.status = :in_play
+  end
+
   def next_move
     # TODO: This is just a placeholder for now...
     self.moves << ['yellow', 0]
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -2,7 +2,9 @@ require 'spec_helper'
 
 describe Game do
   context "when a new game is created" do
-    @game = Game.new
+    before(:each) do
+      @game = Game.new
+    end
 
     it "should have status :in_play" do
       @game.status.should == :in_play
