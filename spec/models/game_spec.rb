3347859e84d77e34301f76fc4a7e6454483cb5ca
Steve Hook <steve.hook@gmail.com>
2011-08-07 19:54:16 +0100

    Added first failing spec for Game model

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe Game do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "when a new game is created" do
+    @game = Game.new
+
+    it "should have status :in_play" do
+      @game.status.should == :in_play
+    end
+  end
 end
