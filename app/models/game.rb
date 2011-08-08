5896ecd622db93d1d29e844ea8061c58f641130d
Steve Hook <steve.hook@gmail.com>
2011-08-08 20:55:52 +0100

    Added migration for winner field in Game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -9,6 +9,54 @@ class Game < ActiveRecord::Base
     self.grid = [[],[],[],[],[],[],[]]
   end
 
+  def evaluate_status
+    evaluate_winner(last_colour)
+    evaluate_game_over
+  end
+
+  def last_colour
+    self.moves.last[0]
+  end
+
+  def evaluate_winner(colour)
+    @winner = colour if has_connect_four(colour)
+  end
+
+  def evaluate_game_over
+    self.status = :finished if self.winner
+  end
+
+  def has_connect_four(colour)
+    has_horizontal_line(colour)
+  end
+
+  def has_horizontal_line(colour)
+    (0..6).each do |x|
+      return true if has_straight_line(colour, x, true)
+    end
+    false
+  end
+
+  def has_straight_line(colour, a, horizonal)
+    counter = 0
+    (0..6).each do |b|
+      value = horizonal ? grid_value(a, b) : grid_value(b, a)
+      if value == colour
+        counter += 1
+      else
+        counter = 0
+      end
+      return true if counter == 4
+    end
+    false
+  end
+
+  def grid_value(x, y)
+    return nil if x < 0 || x >= 7 || y < 0 || y >= 7
+    stack = self.grid[y]
+    stack.length > x ? stack[x] : nil
+  end
+
   def next_move
     # TODO: This is just a placeholder for now...
     self.moves << ['yellow', 0]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110803200324) do
+ActiveRecord::Schema.define(:version => 20110808195434) do
 
   create_table "games", :force => true do |t|
     t.string   "status"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(:version => 20110803200324) do
     t.text     "moves"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "winner"
   end
 
 end
