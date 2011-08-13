12d52b1f52b5b685cc3a28bc5a97597319a677f6
Steve Hook <steve.hook@gmail.com>
2011-08-13 18:20:18 +0100

    Failing specs for testing winning moves

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -24,5 +24,35 @@ describe Player do
       @player.get_next_move(@grid).should == 3
     end
   end
+
+  context "when red has a winning move" do
+    before(:each) do
+      @grid = [
+        [:yellow, :red],
+        [:red, :yellow],
+        [:red, :yellow],
+        [:red, :yellow],
+        [],
+        [],
+        []
+      ]
+    end
+
+    it "no columns should be full" do
+      (0..6).each { |column| @player.is_column_full(@grid, column).should == false }
+    end
+
+    it "column 4 should be a winning move" do
+      @player.is_winning_move(@grid, 4).should == true
+    end
+
+    it "no columns should be saving move" do
+      (0..6).each { |column| @player.is_saving_move(@grid, column).should == false }
+    end
+
+    it "should pick column 4" do
+      @player.get_next_move(@grid).should == 4
+    end
+  end
 end
 
