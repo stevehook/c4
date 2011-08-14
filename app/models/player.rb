af42d756e27d832a1115f142010c9f36c58e8df1
Steve Hook <steve.hook@gmail.com>
2011-08-14 10:06:34 +0100

    Fix specs for testing winning moves

--- app/models/player.rb
+++ app/models/player.rb
@@ -35,7 +35,16 @@ class Player
   end
 
   def is_winning_move(grid, column)
-    false
+    new_grid, row = apply_move(grid, column, @colour)
+    has_connect_four(new_grid, @colour)
+  end
+
+  def apply_move(grid, column, colour = @colour)
+    new_grid = Game.copy_grid(grid)
+    array = new_grid[column]
+    first_empty_row = array.length
+    array << colour
+    return new_grid, first_empty_row
   end
 
   def is_saving_move(grid, column)
@@ -48,5 +57,66 @@ class Player
 
   def sets_up_three_in_row(grid, column)
     false
+  end
+
+  def has_connect_four(grid, colour)
+    has_horizontal_line(grid, colour) || has_vertical_line(grid, colour) || has_diagonal_line(grid, colour)
+  end
+
+  def has_diagonal_line(grid, colour)
+    (-3..3).each do |x|
+      return true if has_diagonal(grid, colour, x, 1)
+    end
+    (3..9).each do |x|
+      return true if has_diagonal(grid, colour, x, -1)
+    end
+    false
+  end
+
+  def has_horizontal_line(grid, colour)
+    (0..6).each do |x|
+      return true if has_straight_line(grid, colour, x, true)
+    end
+    false
+  end
+
+  def has_vertical_line(grid, colour)
+    (0..6).each do |y|
+      return true if has_straight_line(grid, colour, y, false)
+    end
+    false
+  end
+
+  def has_straight_line(grid, colour, a, horizonal)
+    counter = 0
+    (0..6).each do |b|
+      value = horizonal ? grid_value(grid, a, b) : grid_value(grid, b, a)
+      if value == colour
+        counter += 1
+      else
+        counter = 0
+      end
+      return true if counter == 4
+    end
+    false
+  end
+
+  def has_diagonal(grid, colour, x, direction)
+    counter = 0
+    (0..6).each do |y|
+      if grid_value(grid, x + (direction * y), y) == colour
+        counter += 1
+      else
+        counter = 0
+      end
+      return true if counter == 4
+    end
+    false
+  end
+
+  def grid_value(grid, x, y)
+    return nil if x < 0 || x >= 7 || y < 0 || y >= 7
+    stack = grid[y]
+    stack.length > x ? stack[x] : nil
   end
 end
